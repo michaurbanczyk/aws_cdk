2e77f5f40c19ba6d6df6a3c81536159ab6709211
Michal Urbanczyk <michal.urbanczyk@grapeup.com>
2024-05-24 12:03:44 +0200

    api gateway added

--- space-finder/src/services/hello.js
+++ space-finder/src/services/hello.js
@@ -1,4 +1,7 @@
-export const main = async function(event, context){
+exports.main = async function(event, context){
+
+    console.log("Hello we are in the lambda!")
+
     return {
         statusCode: 200,
         body: JSON.stringify('Hello from Lambda!')

965f51c5f3769047f6ee395d6f4640f9b4d4f36f
Michal Urbanczyk <michal.urbanczyk@grapeup.com>
2024-05-24 13:46:14 +0200

    dataStack deployed

--- space-finder/src/infra/stacks/DataStack.ts
+++ space-finder/src/infra/stacks/DataStack.ts
@@ -1,10 +1,24 @@
 import {Stack, StackProps} from "aws-cdk-lib";
 import {Construct} from "constructs";
+import {AttributeType, ITable, Table} from "aws-cdk-lib/aws-dynamodb";
+import {getSuffixFromStack} from "../Utils";
 
 
 export class DataStack extends Stack {
 
+    public readonly spacesTable: ITable
+
     constructor(scope: Construct, id: string, props?: StackProps) {
         super(scope, id, props);
+
+        const suffix = getSuffixFromStack(this)
+
+        this.spacesTable = new Table(this, 'Spaces', {
+            partitionKey: {
+                name: 'id',
+                type: AttributeType.STRING
+            },
+            tableName: `SpaceDynamoTable-${suffix}`
+        })
     }
 }

f46dea35dd5a22e3c097cfac60ab3ed29a71818d
Michal Urbanczyk <michal.urbanczyk@grapeup.com>
2024-05-26 23:07:12 +0200

    lambda with buckets and testing lambda

--- space-finder/src/infra/stacks/LambdaStack.ts
+++ space-finder/src/infra/stacks/LambdaStack.ts
@@ -1,10 +1,11 @@
 import {Stack, StackProps} from "aws-cdk-lib";
 import {Construct} from "constructs";
-import {Code, Runtime} from 'aws-cdk-lib/aws-lambda'
-import { join } from "path";
+import {Runtime} from 'aws-cdk-lib/aws-lambda'
+import {join} from "path";
 import {LambdaIntegration} from "aws-cdk-lib/aws-apigateway";
 import {ITable} from "aws-cdk-lib/aws-dynamodb";
 import {NodejsFunction} from "aws-cdk-lib/aws-lambda-nodejs";
+import {Effect, PolicyStatement} from "aws-cdk-lib/aws-iam";
 
 interface LambdaStackProps extends StackProps {
     spacesTable: ITable
@@ -26,6 +27,15 @@ export class LambdaStack extends Stack {
                 TABLE_NAME: props.spacesTable.tableName
             }
         })
+
+        helloLambda.addToRolePolicy(new PolicyStatement({
+            effect: Effect.ALLOW,
+            actions: [
+                's3:ListAllMyBuckets',
+                's3:ListBucket'
+            ],
+            resources: ["*"]
+        }))
 
         this.helloLambdaIntegration = new LambdaIntegration(helloLambda)
     }
